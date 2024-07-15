b7967e082410ae042e62ce01fac9a9f99cee6e7a
Deema-Thnabi <deemathnabi25@gmail.com>
2024-07-15 18:40:52 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+    const name = 'Deema Thnabi';
+    const age = 22;
+
+  return (
+    <>
+    <h1>Hello {name}</h1>
+    <h2>Your age is {age}</h2>
+    </>
+  )
+}
+
+export default App
